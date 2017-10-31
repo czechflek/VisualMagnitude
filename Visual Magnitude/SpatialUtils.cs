e74328462fe2859373674730f5408fd68fba92c1
Jakub Čech <kuba7593@gmail.com>
2017-10-31 14:40:15 -0500

    OpenCl initial

--- Visual Magnitude/SpatialUtils.cs
+++ Visual Magnitude/SpatialUtils.cs
@@ -1,12 +1,17 @@
 ﻿using Cudafy;
+using Cudafy.Host;
+using Cudafy.Translator;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Visual_Magnitude {
-    class SpatialUtils {
+namespace Visual_Magnitude
+{
+
+    class SpatialUtils
+    {
         public enum Orientation { N, NNE, NE, ENE, E, ESE, ES, SSE, S, SSW, SW, WSW, W, WNW, NW, NNW }
 
         private static Dictionary<Orientation, LosCells> losCellsDict;
@@ -17,7 +22,8 @@ namespace Visual_Magnitude {
         /// <summary>
         /// Definitions of positions of neighbor cells in each of 16 sectors.
         /// </summary>
-        static SpatialUtils() {
+        static SpatialUtils()
+        {
             losCellsDict = new Dictionary<Orientation, LosCells>();
             losCellsDict.Add(Orientation.W, new LosCells(0, 1, 0, 1));
             losCellsDict.Add(Orientation.WNW, new LosCells(0, 1, 1, 1));
@@ -37,21 +43,48 @@ namespace Visual_Magnitude {
             losCellsDict.Add(Orientation.WSW, new LosCells(0, 1, -1, 1));
         }
 
+        public static void Calculate()
+        {
+            GPGPU gpu = CudafyHost.GetDevice(eGPUType.OpenCL);
+            CudafyModule km = CudafyTranslator.Cudafy(eArchitecture.OpenCL);
+
+            gpu.LoadModule(km);
+
+            MockMap elevationMock = new MockMap(100, 200);
+            double[,] elevationGpu = gpu.CopyToDevice(elevationMock.Map);
+            MockMap losMock = new MockMap(100, 200);
+            double[,] losGpu = gpu.CopyToDevice(losMock.Map);
+            bool[] resultGpu = gpu.Allocate<bool>(100);
+            SpatialUtils.ViewpointProps viewpoint = new SpatialUtils.ViewpointProps(20, 30, 1.3, 230);
+
+            System.Diagnostics.Debug.WriteLine("Initial: {0}", losMock.Map[8, 10]);
+            gpu.Launch(1, 1, "TestMethod", 1, elevationGpu, losGpu, viewpoint, 8, 10, Orientation.NW, resultGpu);
+
+            double[,] losLocal = new double[100, 200];
+            gpu.CopyFromDevice(losGpu, losLocal);
+            System.Diagnostics.Debug.WriteLine("Final: {0}", losLocal[8, 10]);
+
+            gpu.FreeAll();
+        }
 
         [Cudafy]
-        public static void IsCellVisible(int threadId, double[][] elevationMap, double[][] losMap, ViewpointProps viewpoint, int cellY, int cellX, Orientation cellOrientation, bool[] visible) {
+        public static void IsCellVisible(int threadId, double[,] elevationMap, double[,] losMap, ViewpointProps viewpoint, int cellY, int cellX, Orientation cellOrientation, bool[] visible)
+        {
             GetNeighborCells(cellY, cellX, cellOrientation, out int adjacentY, out int adjacentX, out int offsetY, out int offsetX);
 
             double adjacentWeight = InterpolateWeight(viewpoint, cellY, cellX, cellOrientation);
 
             double viewingLos = GetViewingSlope(elevationMap, viewpoint, cellY, cellX);
-            double cellLos = losMap[adjacentY][adjacentX] * adjacentWeight + losMap[offsetY][offsetX] * (1 - adjacentWeight);
+            double cellLos = losMap[adjacentY, adjacentX] * adjacentWeight + losMap[offsetY, offsetX] * (1 - adjacentWeight);
 
-            if (viewingLos < cellLos) {
-                losMap[cellY][cellX] = cellLos;
+            if (viewingLos < cellLos)
+            {
+                losMap[cellY, cellX] = cellLos;
                 visible[threadId] = false;
-            } else {
-                losMap[cellY][cellX] = viewingLos;
+            }
+            else
+            {
+                losMap[cellY, cellX] = viewingLos;
                 visible[threadId] = true;
             }
         }
@@ -65,7 +98,8 @@ namespace Visual_Magnitude {
         /// <param name="cellOrientation">Orientation of the cell</param>
         /// <returns>Weight of the adjacent cell</returns>
         [Cudafy]
-        private static double InterpolateWeight(ViewpointProps viewpoint, int cellY, int cellX, Orientation cellOrientation) {
+        private static double InterpolateWeight(ViewpointProps viewpoint, int cellY, int cellX, Orientation cellOrientation)
+        {
             GetNeighborCells(cellY, cellX, cellOrientation, out int adjacentY, out int adjacentX, out int offsetY, out int offsetX);
 
             float cellAspect = (float)GetViewingAspect(viewpoint, viewpoint.Y, viewpoint.X);
@@ -88,16 +122,18 @@ namespace Visual_Magnitude {
         /// <param name="cellX">X coordinate of the cell</param>
         /// <returns>Angle (radians)</returns>
         [Cudafy]
-        private static double GetViewingAspect(ViewpointProps viewpoint, int cellY, int cellX) {
+        private static double GetViewingAspect(ViewpointProps viewpoint, int cellY, int cellX)
+        {
             return GMath.Atan2((cellY - viewpoint.Y) * viewpoint.CellResoulution, (cellX - viewpoint.X) * viewpoint.CellResoulution);
         }
 
         [Cudafy]
-        private static double GetViewingSlope(double[][] elevationMap, ViewpointProps viewpoint, int cellY, int cellX) {
-            float[] yxzDistances = GetYXZDistances(viewpoint, cellY, cellX, elevationMap[cellY][cellX]);
-            float directDistance = GMath.Sqrt(GMath.Pow(yxzDistances[1], 2) + GMath.Pow(yxzDistances[0], 2));
+        private static double GetViewingSlope(double[,] elevationMap, ViewpointProps viewpoint, int cellY, int cellX)
+        {
+            GetYXZDistances(viewpoint, cellY, cellX, elevationMap[cellY, cellX], out float distY, out float distX, out float distZ);
+            float directDistance = GMath.Sqrt(GMath.Pow(distX, 2) + GMath.Pow(distY, 2));
 
-            return GMath.Atan2(yxzDistances[2], directDistance);
+            return GMath.Atan2(distZ, directDistance);
         }
 
         /// <summary>
@@ -109,18 +145,18 @@ namespace Visual_Magnitude {
         /// <param name="cellElevation">Elevation of the cell</param>
         /// <returns></returns>
         [Cudafy]
-        private static float[] GetYXZDistances(ViewpointProps viewpoint, int cellY, int cellX, double cellElevation) {
-            float distX = GMath.Abs(viewpoint.X - cellX) * viewpoint.CellResoulution;
-            float distY = GMath.Abs(viewpoint.Y - cellY) * viewpoint.CellResoulution;
+        private static void GetYXZDistances(ViewpointProps viewpoint, int cellY, int cellX, double cellElevation, out float distY, out float distX, out float distZ)
+        {
+            distX = GMath.Abs(viewpoint.X - cellX) * viewpoint.CellResoulution;
+            distY = GMath.Abs(viewpoint.Y - cellY) * viewpoint.CellResoulution;
 
             double curvature = (GMath.Pow(distX, 2) + GMath.Pow(distY, 2)) / earthDiameter;
-            float distZ = (float)(cellElevation - curvature + lightRefraction * curvature - viewpoint.Elevation);
-
-            return new float[3] { distY, distX, distZ };
+            distZ = (float)(cellElevation - curvature + lightRefraction * curvature - viewpoint.Elevation);
         }
 
         [Cudafy]
-        private static void GetNeighborCells(int cellY, int cellX, Orientation cellOrientation, out int adjacentY, out int adjacentX, out int offsetY, out int offsetX) {
+        private static void GetNeighborCells(int cellY, int cellX, Orientation cellOrientation, out int adjacentY, out int adjacentX, out int offsetY, out int offsetX)
+        {
             losCellsDict.TryGetValue(cellOrientation, out LosCells losCells);
             adjacentX = cellX + losCells.XCell1;
             adjacentY = cellY + losCells.YCell1;
@@ -128,13 +164,15 @@ namespace Visual_Magnitude {
             offsetY = cellY + losCells.YCell2;
         }
 
-        private struct LosCells {
+        private struct LosCells
+        {
             int yCell1;
             int xCell1;
             int yCell2;
             int xCell2;
 
-            public LosCells(int yCell1, int xCell1, int yCell2, int xCell2) : this() {
+            public LosCells(int yCell1, int xCell1, int yCell2, int xCell2) : this()
+            {
                 this.yCell1 = yCell1;
                 this.xCell1 = xCell1;
                 this.yCell2 = yCell2;
@@ -150,13 +188,15 @@ namespace Visual_Magnitude {
             public int XCell2 { get; set; }
         }
 
-        public class ViewpointProps {
+        public class ViewpointProps
+        {
             int y;
             int x;
             double cellResoulution;
             double elevation;
 
-            public ViewpointProps(int y, int x, double cellResoulution, double elevation) {
+            public ViewpointProps(int y, int x, double cellResoulution, double elevation)
+            {
                 this.y = y;
                 this.x = x;
                 this.cellResoulution = cellResoulution;
@@ -171,5 +211,26 @@ namespace Visual_Magnitude {
 
             public int Elevation { get; set; }
         }
+    }
+
+    class MockMap
+    {
+        double[,] map;
+
+        public MockMap(int Y, int X)
+        {
+            Map = new double[Y, X];
+
+            Random rnd = new Random();
+            for (int i = 0; i < Y; i++)
+            {
+                for (int j = 0; j < X; j++)
+                {
+                    Map[i, j] = rnd.NextDouble() * rnd.Next(300, 1000);
+                }
+            }
+        }
+
+        public double[,] Map { get => map; set => map = value; }
     }
 }
--- Visual Magnitude/StartButton.cs
+++ Visual Magnitude/StartButton.cs
@@ -9,23 +9,22 @@ using Cudafy;
 using Cudafy.Translator;
 using Cudafy.Host;
 
-namespace Visual_Magnitude {
-    internal class StartButton : Button {
-        protected override void OnClick() {
+namespace Visual_Magnitude
+{
+    class StartButton : Button
+    {
+        protected override void OnClick()
+        {
             ArcGIS.Desktop.Framework.Dialogs.MessageBox.Show("Started");
 
             System.Diagnostics.Debug.WriteLine("************************************************************************");
 
-            GPGPU gpu = CudafyHost.GetDevice(eGPUType.OpenCL);
-            CudafyModule km = CudafyTranslator.Cudafy(eArchitecture.OpenCL);
-            
-            gpu.LoadModule(km);
-
-            gpu.Launch().Kernel();
+            SpatialUtils.Calculate();
         }
 
         [Cudafy]
-        public static void Kernel() {
-        }
+        public static void Kernel()
+        {
+        }        
     }
 }
